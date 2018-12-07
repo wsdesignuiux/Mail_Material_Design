dca1a05fb2369794c7db46d12b658706ca9c6736
Rushabh Patel <wsdesignuiux@gmail.com>
2018-12-07 15:21:50 +0530

    Initial commit

---
+++ app/src/main/java/com/example/wolfsoft3/mail_material_design/Mail.java
@@ -0,0 +1,13 @@
+package com.example.wolfsoft3.mail_material_design;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Mail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mail);
+    }
+}
